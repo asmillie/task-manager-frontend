2a064482dc819c788516c7e1d7580b19f14934b5
Alan Smillie <alan.smillie@gmail.com>
2020-05-22 13:35:42 -0400

    Add Api Url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  taskApi: {
+      url: 'http://localhost:3000',
+      endpoint: {
+          user: {
+              signup: '/signup',
+          }
+      }
+  }
 };
 
 /*
