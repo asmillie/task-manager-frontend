995696f9d133e0ff10b66d96e3e0b8106b11b002
Alan Smillie <alan.smillie@gmail.com>
2020-05-22 13:36:38 -0400

    Implement basic signup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,22 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { UserService } from '../user/user.service';
+import { Subscription } from 'rxjs';
+import { User } from '../user/user';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit, OnDestroy {
 
   signupForm;
   isLoading = false;
+  signupSub: Subscription;
 
   constructor(
     private fb: FormBuilder,
+    private userService: UserService,
   ) { }
 
   ngOnInit(): void {
     this.initForm();
+  }
+
+  ngOnDestroy(): void {
+    this.signupSub.unsubscribe();
   }
 
   onSubmit() {
@@ -28,16 +37,27 @@ export class SignupComponent implements OnInit {
       return;
     }
 
+    const name = this.signupForm.get('name').value;
     const email = this.signupForm.get('email').value;
     const password = this.signupForm.get('password').value;
 
     // TODO: Add Validation
-    // TODO: Call authService
     console.log(`Form Submitted: Email => ${email}, Password => ${password}`);
+    this.signupSub = this.userService.signup(name, email, password)
+      .subscribe((user: User) => {
+        this.isLoading = false;
+        console.log(`User Signup Success: ${JSON.stringify(user)}`);
+      });
   }
   // TODO: Async Validation of Unique Email Address
   private initForm() {
     this.signupForm = this.fb.group({
+      name: [{
+        value: '',
+        disabled: this.isLoading,
+        },
+        [ Validators.required ],
+      ],
       email: [{
           value: '',
           disabled: this.isLoading,
--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,12 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
+import { User, IUser } from './user';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { map } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  private USER_URL = '';
+  private API_URL = environment.taskApi.url;
+  private USER_SIGNUP = environment.taskApi.endpoint.user.signup;
 
   constructor(private http: HttpClient) { }
+
+  signup(name: string, email: string, password: string): Observable<User> {
+    return this.http.post<IUser>(
+      this.API_URL + this.USER_SIGNUP,
+      {
+        name,
+        email: {
+          address: email,
+        },
+        password,
+      }
+    ).pipe(
+      map((user) => {
+        if (user) {
+          return new User(user.name, user.email.address, user._id);
+        }
+
+        return null;
+      })
+    );
+  }
 }
