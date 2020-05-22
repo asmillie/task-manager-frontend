415f2feff8c7320d8b271eeb4526a3f863fc835b
Alan Smillie <alan.smillie@gmail.com>
2020-05-22 11:21:34 -0400

    Create User Class

---
+++ src/app/user/user.ts
@@ -0,0 +1,77 @@
+import { Url } from 'url';
+
+export interface IUser {
+    id?: string;
+    name: string;
+    email: string;
+    avatarUrl?: Url;
+    token?: string;
+}
+
+export class User {
+    private _id: string;
+    private _name: string;
+    private _email: string;
+    private _avatarUrl: Url;
+    private _token: string;
+
+    constructor(
+        name: string,
+        email: string,
+        id?: string,
+        token?: string,
+        avatarUrl?: Url) {
+            this._name = name;
+            this._email = email;
+            if (id) {
+                this._id = id;
+            }
+            if (token) {
+                this._token = token;
+            }
+            if (avatarUrl) {
+                this._avatarUrl = avatarUrl;
+            }
+        }
+
+    get id(): string {
+        return this._id;
+    }
+
+    set id(id: string) {
+        this._id = id;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    set name(name: string) {
+        this._name = name;
+    }
+
+    get email(): string {
+        return this._email;
+    }
+
+    set email(email: string) {
+        this._email = email;
+    }
+
+    get token(): string {
+        return this._token;
+    }
+
+    set token(token: string) {
+        this._token = token;
+    }
+
+    get avatarUrl(): Url {
+        return this._avatarUrl;
+    }
+
+    set avatarUrl(url: Url) {
+        this._avatarUrl = url;
+    }
+
+}
