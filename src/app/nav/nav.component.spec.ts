3ea1c86f0b56b75eb6ae74921d58eca4ec525a62
Alan Smillie <alan.smillie@gmail.com>
2020-06-04 13:48:34 -0400

    Update tests for Jest

--- src/app/nav/nav.component.spec.ts
+++ src/app/nav/nav.component.spec.ts
@@ -20,6 +20,6 @@ describe('NavComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeDefined();
   });
 });
--- src/app/shared/loading-spinner/loading-spinner.component.spec.ts
+++ src/app/shared/loading-spinner/loading-spinner.component.spec.ts
@@ -20,6 +20,6 @@ describe('LoadingSpinnerComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeDefined();
   });
 });
