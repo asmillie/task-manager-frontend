ff5abd190a5671e1f71552d885024bb97094dce2
Alan Smillie <alan.smillie@gmail.com>
2020-06-05 13:04:54 -0400

    Add shared module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [AuthComponent, LoginComponent],
   imports: [
-    CommonModule
-  ]
+    SharedModule,
+  ],
+  exports: [LoginComponent]
 })
 export class AuthModule { }
