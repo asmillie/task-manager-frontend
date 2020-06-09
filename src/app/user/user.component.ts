cdb406f09028e9d501e46bf2e1606b1414e7ed0c
Alan Smillie <alan.smillie@gmail.com>
2020-06-09 11:07:44 -0400

    Get user from resolver service

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { User } from './user';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  userSub: Subscription;
+  user: User;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.initUser();
   }
 
+  ngOnDestroy(): void {
+    if (this.userSub) {
+      this.userSub.unsubscribe();
+    }
+  }
+
+  private initUser(): void {
+    this.userSub = this.route.data.subscribe(
+      (data: { user: User }) => {
+        this.user = data.user;
+      });
+  }
 }
