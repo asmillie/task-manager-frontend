69f240b8717731fa410ab7dbcc350101493bb962
Alan Smillie <alan.smillie@gmail.com>
2020-06-09 11:20:55 -0400

    Add Form

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { User } from './user';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
@@ -12,11 +13,16 @@ export class UserComponent implements OnInit, OnDestroy {
 
   userSub: Subscription;
   user: User;
+  userForm: FormGroup;
+  isLoading = false;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.initUser();
+    this.initForm();
   }
 
   ngOnDestroy(): void {
@@ -25,10 +31,31 @@ export class UserComponent implements OnInit, OnDestroy {
     }
   }
 
+  onSubmit(): void {
+    console.log(this.userForm);
+  }
+
   private initUser(): void {
     this.userSub = this.route.data.subscribe(
       (data: { user: User }) => {
         this.user = data.user;
       });
+  }
+
+  private initForm(): void {
+    this.userForm = this.fb.group({
+      name: [{
+        value: this.user.name,
+        disabled: this.isLoading,
+      }, {
+          validators: [Validators.required]
+      }],
+      email: [{
+        value: this.user.email,
+        disabled: this.isLoading,
+      }, {
+        validators: [Validators.required, Validators.email]
+      }],
+    });
   }
 }
