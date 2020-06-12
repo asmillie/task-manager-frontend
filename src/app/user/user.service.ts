ebe6e29063676732e1c6176e334c3c5b6edaef8b
Alan Smillie <alan.smillie@gmail.com>
2020-06-12 14:05:24 -0400

    Add update method

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -2,9 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 import { User, IUser } from './class/user';
-import { Observable, throwError } from 'rxjs';
+import { Observable, throwError, of } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { map, catchError } from 'rxjs/operators';
+import { map, catchError, tap, switchMap, take } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
+import { UserUpdateOpts } from './class/user-update-opts';
 
 @Injectable({
   providedIn: 'root'
@@ -13,9 +15,12 @@ export class UserService {
 
   private API_URL = environment.taskApi.url;
   private USER_SIGNUP = environment.taskApi.endpoint.signup;
+  private USER_UPDATE = environment.taskApi.endpoint.user.patch;
   private EMAIL_EXISTS = environment.taskApi.endpoint.emailExists;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService) {}
 
   signup$(name: string, email: string, password: string): Observable<User | string> {
     return this.http.post<IUser>(
@@ -53,8 +58,23 @@ export class UserService {
     );
   }
 
-  update$(): Observable<User | string> {
-    // TODO: implement
+  update$(userUpdateOpts: UserUpdateOpts, token: string): Observable<User | string> {
+    return this.http.patch<IUser>(
+      this.API_URL + this.USER_UPDATE,
+      { userUpdateOpts }
+    ).pipe(
+      catchError(this.handleError),
+      map((user: IUser) => {
+        if (!user) {
+          return null;
+        }
+
+        return new User(user.name, user.email.address, user._id, token);
+      }),
+      tap((user: User) => {
+        this.authService.userSubject.next(user);
+      }),
+    );
   }
 
   private handleError(error: HttpErrorResponse): Observable<never> {
