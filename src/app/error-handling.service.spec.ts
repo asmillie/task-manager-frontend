c89652e9773c3bbbc7e038d9956208b7d853186c
Alan Smillie <alan.smillie@gmail.com>
2020-06-24 13:54:57 -0400

    Add unit tests

--- src/app/error-handling.service.spec.ts
+++ src/app/error-handling.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ErrorHandlingService } from './error-handling.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ValidationError } from 'class-validator';
 
 describe('ErrorHandlingService', () => {
   let service: ErrorHandlingService;
@@ -12,5 +14,41 @@ describe('ErrorHandlingService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('handleHttpError$', () => {
+    it('should return formatted error message', (done) => {
+      const error = new HttpErrorResponse({
+        status: 500,
+        error: { message: 'Internal Server Exception' }
+      });
+
+      service.handleHttpError$(error).subscribe({
+        error: (result) => {
+          expect(result).toEqual(`API Error: ${error.error.message}, Status Code: ${error.status}`);
+          done();
+        }
+      });
+    });
+  });
+
+  describe('handleValidationError$', () => {
+    it('should return formatted error message', (done) => {
+      const vErrorOne = new ValidationError();
+      vErrorOne.property = 'completed';
+      vErrorOne.constraints = { required: 'Field is required' };
+      const vErrorTwo = new ValidationError();
+      vErrorTwo.property = 'skip';
+      vErrorTwo.constraints = { int: 'Must be an integer' };
+      const vErrors: ValidationError[] = [vErrorOne, vErrorTwo];
+
+      service.handleValidationError$(vErrors).subscribe({
+        error: (result) => {
+          expect(result).toContain(vErrorOne.constraints.required);
+          expect(result).toContain(vErrorTwo.constraints.int);
+          done();
+        }
+      });
+    });
   });
 });
