201ee9784df3f2ee1f52cd40c617c083d7e888cc
Alan Smillie <alan.smillie@gmail.com>
2020-07-02 10:07:47 -0400

    Add unit tests for add method

--- src/app/tasks/tasks.service.spec.ts
+++ src/app/tasks/tasks.service.spec.ts
@@ -9,6 +9,7 @@ import { of, throwError } from 'rxjs';
 import { mockTasks, mockCompletedTasks } from '../../mocks/mock-tasks';
 import { TaskQueryOptions } from './task-query-options';
 import { environment } from '../../environments/environment';
+import { Task } from './task';
 
 describe('TasksService', () => {
   let service: TasksService;
@@ -29,7 +30,7 @@ describe('TasksService', () => {
 
   describe('getAll$', () => {
     const API_URL = environment.taskApi.url;
-    const TASKS_ENDPOINT = environment.taskApi.endpoint.tasks;
+    const GET_TASKS = environment.taskApi.endpoint.tasks.get;
 
     it('should return list of tasks', (done) => {
       mockHttpService.post.mockReturnValueOnce(of(mockTasks));
@@ -45,7 +46,7 @@ describe('TasksService', () => {
 
     it('should add completed param to http request', (done) => {
       mockHttpService.post.mockReturnValueOnce(of(mockCompletedTasks));
-      const url = `${API_URL}${TASKS_ENDPOINT}?completed=true`;
+      const url = `${API_URL}${GET_TASKS}?completed=true`;
 
       service.getAll$(true).subscribe(_ => {
         expect(mockHttpService.post).toHaveBeenCalledWith(url, undefined);
@@ -55,7 +56,7 @@ describe('TasksService', () => {
 
     it('should add query options to http request', (done) => {
       mockHttpService.post.mockReturnValueOnce(of(mockTasks));
-      const url = `${API_URL}${TASKS_ENDPOINT}`;
+      const url = `${API_URL}${GET_TASKS}`;
 
       const tqo: TaskQueryOptions = {};
       tqo.limit = 5;
@@ -70,18 +71,44 @@ describe('TasksService', () => {
         done();
       });
     });
+
+    it('should return error message', (done) => {
+      const httpError = new HttpErrorResponse({ error: 'Internal Server Error', status: 500 });
+      mockHttpService.post.mockReturnValueOnce(throwError(httpError));
+      mockErrorHandlingService.handleHttpError$.mockReturnValueOnce(throwError('error'));
+
+      service.getAll$().subscribe({
+        error: (err) => {
+          expect(err).toEqual('error');
+          done();
+        }
+      });
+    });
   });
 
-  it('should return error message', (done) => {
-    const httpError = new HttpErrorResponse({ error: 'Internal Server Error', status: 500 });
-    mockHttpService.post.mockReturnValueOnce(throwError(httpError));
-    mockErrorHandlingService.handleHttpError$.mockReturnValueOnce(throwError('error'));
+  describe('add$', () => {
+    it('should return task on successful post request', (done) => {
+      const mockTask = new Task('owner-id', 'description', false);
+      mockHttpService.post.mockReturnValueOnce(of(mockTask));
 
-    service.getAll$().subscribe({
-      error: (err) => {
-        console.log(err);
+      service.add$(mockTask).subscribe(result => {
+        expect(result).toBeInstanceOf(Task);
+        expect(result).toEqual(mockTask);
         done();
-      }
+      });
+    });
+
+    it('should return error message', (done) => {
+      const httpError = new HttpErrorResponse({ error: 'Internal Server Error', status: 500 });
+      mockHttpService.post.mockReturnValueOnce(throwError(httpError));
+      mockErrorHandlingService.handleHttpError$.mockReturnValueOnce(throwError('error'));
+
+      service.add$(mockTasks[0]).subscribe({
+        error: (err) => {
+          expect(err).toEqual('error');
+          done();
+        }
+      });
     });
   });
 });
