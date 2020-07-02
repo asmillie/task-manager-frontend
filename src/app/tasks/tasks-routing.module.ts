3ee5c272e0ef7b476c17d61792be85cdb1ef0bee
Alan Smillie <alan.smillie@gmail.com>
2020-07-02 11:21:28 -0400

    Add AuthGuard

--- src/app/tasks/tasks-routing.module.ts
+++ src/app/tasks/tasks-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TasksComponent } from './tasks.component';
+import { AuthGuard } from '../auth/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
+    canActivate: [AuthGuard],
     component: TasksComponent
   }
 ];
