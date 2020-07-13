9986eb22e30ee1e32b7ea53d314f498cf5cb5a9e
Alan Smillie <alan.smillie@gmail.com>
2020-07-13 09:34:48 -0400

    Add search tasks

--- src/app/tasks/search-tasks/search-tasks.component.ts
+++ src/app/tasks/search-tasks/search-tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TasksService } from '../tasks.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-search-tasks',
@@ -10,16 +10,75 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class SearchTasksComponent implements OnInit {
 
   searchForm: FormGroup;
+  isLoading = false;
 
   constructor(
     private tasksService: TasksService,
     private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  onSubmit(): void {
+    console.log(this.searchForm);
+    if (this.searchForm.status !== 'VALID') {
+      return;
+    }
+
+    // TODO: Search query
   }
 
   private initForm(): void {
-
+    this.searchForm = this.fb.group({
+      description: [{
+        value: '',
+        disabled: this.isLoading,
+      }],
+      start_createdAt: [{
+        value: '',
+        disabled: this.isLoading,
+      }],
+      end_createdAt: [{
+        value: '',
+        disabled: this.isLoading,
+      }],
+      start_updatedAt: [{
+        value: '',
+        disabled: this.isLoading,
+      }],
+      end_updatedAt: [{
+        value: '',
+        disabled: this.isLoading,
+      }],
+      completed: [{
+        value: 'all',
+        disabled: this.isLoading,
+      }]
+    });
   }
 
+  get description(): AbstractControl {
+    return this.searchForm.get('description');
+  }
+
+  get startCreatedAt(): AbstractControl {
+    return this.searchForm.get('start_createdAt');
+  }
+
+  get endCreatedAt(): AbstractControl {
+    return this.searchForm.get('end_createdAt');
+  }
+
+  get startUpdatedAt(): AbstractControl {
+    return this.searchForm.get('start_updatedAt');
+  }
+
+  get endUpdatedAt(): AbstractControl {
+    return this.searchForm.get('end_updatedAt');
+  }
+
+  get completed(): AbstractControl {
+    return this.searchForm.get('completed');
+  }
 }
--- src/app/tasks/tasks-routing.module.ts
+++ src/app/tasks/tasks-routing.module.ts
@@ -3,12 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { TasksComponent } from './tasks.component';
 import { AuthGuard } from '../auth/auth.guard';
 import { UserResolverService } from '../user/user-resolver.service';
+import { SearchTasksComponent } from './search-tasks/search-tasks.component';
 
 const routes: Routes = [
   {
     path: '',
     canActivate: [AuthGuard],
     component: TasksComponent
+  },
+  {
+    path: 'search',
+    canActivate: [AuthGuard],
+    component: SearchTasksComponent
   }
 ];
 
