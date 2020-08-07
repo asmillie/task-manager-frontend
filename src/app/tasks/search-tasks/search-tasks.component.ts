bc20923a3d8d04dddb5d597cd4cea13a9b9dc361
Alan Smillie <alan.smillie@gmail.com>
2020-08-07 14:12:09 -0400

    Preserve sort and pagination on query options

--- src/app/tasks/search-tasks/search-tasks.component.ts
+++ src/app/tasks/search-tasks/search-tasks.component.ts
@@ -1,110 +1,158 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TasksService } from '../tasks.service';
 import { FormBuilder, FormGroup, AbstractControl } from '@angular/forms';
 import { TaskQueryOptions } from '../task-query-options';
 import { MIN_TASK_DATE } from '../../constants';
 import { dateValidator } from '../../shared/date.validator';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-search-tasks',
   templateUrl: './search-tasks.component.html',
   styleUrls: ['./search-tasks.component.css']
 })
-export class SearchTasksComponent implements OnInit {
+export class SearchTasksComponent implements OnInit, OnDestroy {
 
   searchForm: FormGroup;
   isLoading = false;
   minDate = MIN_TASK_DATE;
+  errorMessage = '';
+  searchSub: Subscription;
+  tqo: TaskQueryOptions;
 
   constructor(
     private tasksService: TasksService,
     private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.initTaskQueryOptions();
     this.initForm();
   }
 
+  ngOnDestroy(): void {
+    if (this.searchSub) {
+      this.searchSub.unsubscribe();
+    }
+  }
+
   onSubmit(): void {
-    console.log(this.searchForm);
     if (this.searchForm.status !== 'VALID') {
       return;
     }
 
+    this.errorMessage = '';
+    this.isLoading = true;
+
     const taskQueryOpts = this.getTaskQueryOpts();
-    if (taskQueryOpts) {
-      this.tasksService.taskQueryOptions.next(taskQueryOpts);
-    }
+    this.tasksService.taskQueryOptions.next(taskQueryOpts);
+
+    this.searchSub = this.tasksService.search$().subscribe((tasks) => {
+      this.isLoading = false;
+    }, (err) => {
+      this.isLoading = false;
+      this.errorMessage = err;
+    });
   }
 
   resetForm(): void {
-    this.searchForm.reset({ complete: true, incomplete: true });
+    this.searchForm.reset({
+      complete: true,
+      incomplete: true,
+      start_createdAt: '',
+      end_createdAt: '',
+      start_updatedAt: '',
+      end_updatedAt: '',
+    });
   }
 
-  private getTaskQueryOpts(): TaskQueryOptions | null {
-    const taskQueryOpts: TaskQueryOptions = {};
+  private getTaskQueryOpts(): TaskQueryOptions {
+    const taskQueryOpts: TaskQueryOptions = this.tqo;
     if (this.complete.value && !this.incomplete.value) {
       taskQueryOpts.completed = true;
     } else if (!this.complete.value && this.incomplete.value) {
       taskQueryOpts.completed = false;
+    } else if (taskQueryOpts.completed) {
+      delete taskQueryOpts.completed;
     }
 
     if (this.startCreatedAt.value !== '' && this.startCreatedAt.valid) {
-      taskQueryOpts.startCreatedAt = this.startCreatedAt.value;
+      taskQueryOpts.startCreatedAt = new Date(this.startCreatedAt.value);
+    } else if (taskQueryOpts.startCreatedAt) {
+      delete taskQueryOpts.startCreatedAt;
     }
 
     if (this.endCreatedAt.value !== '' && this.endCreatedAt.valid) {
-      taskQueryOpts.endCreatedAt = this.endCreatedAt.value;
+      const eca = new Date(this.endCreatedAt.value);
+      eca.setUTCHours(23, 59, 59, 999);
+      taskQueryOpts.endCreatedAt = eca;
+    } else if (taskQueryOpts.endCreatedAt) {
+      delete taskQueryOpts.endCreatedAt;
     }
 
     if (this.startUpdatedAt.value !== '' && this.startUpdatedAt.valid) {
-      taskQueryOpts.startUpdatedAt = this.startUpdatedAt.value;
+      taskQueryOpts.startUpdatedAt = new Date(this.startUpdatedAt.value);
+    } else if (taskQueryOpts.startUpdatedAt) {
+      delete taskQueryOpts.startUpdatedAt;
     }
 
     if (this.endUpdatedAt.value !== '' && this.endUpdatedAt.valid) {
-      taskQueryOpts.endUpdatedAt = this.endUpdatedAt.value;
+      const eua = new Date(this.endUpdatedAt.value);
+      eua.setUTCHours(23, 59, 59, 999);
+      taskQueryOpts.endUpdatedAt = eua;
+    } else if (taskQueryOpts.endUpdatedAt) {
+      delete taskQueryOpts.endUpdatedAt;
     }
 
-    if (Object.entries(taskQueryOpts).length === 0) {
-      return null;
-    }
     return taskQueryOpts;
   }
 
   private initForm(): void {
     this.searchForm = this.fb.group({
-      start_createdAt: [{
-        value: '',
-        disabled: this.isLoading,
-      }, {
-        validators: dateValidator
-      }],
-      end_createdAt: [{
-        value: '',
-        disabled: this.isLoading,
-      }, {
-        validators: dateValidator
-      }],
-      start_updatedAt: [{
-        value: '',
-        disabled: this.isLoading,
-      }, {
-        validators: dateValidator
-      }],
-      end_updatedAt: [{
-        value: '',
-        disabled: this.isLoading,
-      }, {
-        validators: dateValidator
-      }],
-      complete: [{
-        value: true,
-        disabled: this.isLoading,
-      }],
-      incomplete: [{
-        value: true,
-        disabled: this.isLoading
-      }]
+        start_createdAt: [{
+          value: '',
+          disabled: this.isLoading,
+        }, {
+          validators: dateValidator
+        }],
+        end_createdAt: [{
+          value: '',
+          disabled: this.isLoading,
+        }, {
+          validators: dateValidator
+        }],
+        start_updatedAt: [{
+          value: '',
+          disabled: this.isLoading,
+        }, {
+          validators: dateValidator
+        }],
+        end_updatedAt: [{
+          value: '',
+          disabled: this.isLoading,
+        }, {
+          validators: dateValidator
+        }],
+        complete: [{
+          value: true,
+          disabled: this.isLoading,
+        }],
+        incomplete: [{
+          value: true,
+          disabled: this.isLoading
+        }]
+     });
+  }
+
+  private initTaskQueryOptions(): void {
+    const tqoSub = this.tasksService.taskQueryOptions.subscribe(tqo => {
+      this.tqo = tqo;
     });
+
+    if (this.searchSub) {
+      this.searchSub.add(tqoSub);
+    } else {
+      this.searchSub = tqoSub;
+    }
   }
 
   get startCreatedAt(): AbstractControl {
