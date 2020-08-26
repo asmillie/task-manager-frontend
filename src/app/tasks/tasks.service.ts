1c0d582a39a1f36f4096a72752a1aa144b4c7fbb
Alan Smillie <alan.smillie@gmail.com>
2020-08-26 11:09:24 -0400

    Remove logging

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -34,8 +34,6 @@ export class TasksService {
         if (!taskQueryOpts) {
           return null;
         }
-        console.log(`TasksService.search$: \n`);
-        console.log(JSON.stringify(taskQueryOpts));
 
         this.tasksLoading.next(true);
 
