303ccf5cc124778f29f4c07c05726d1e733c9972
Alan Smillie <alan.smillie@gmail.com>
2020-08-26 11:09:12 -0400

    Reflect change to backend query options (paginate)

--- src/app/tasks/task-query-options.ts
+++ src/app/tasks/task-query-options.ts
@@ -1,8 +1,8 @@
 import { TaskSortOption } from './task-sort-option';
 
 export interface TaskQueryOptions {
-    limit?: number;
-    skip?: number;
+    limit: number;
+    page: number;
     sort?: TaskSortOption[];
     completed?: boolean;
     startCreatedAt?: Date;
