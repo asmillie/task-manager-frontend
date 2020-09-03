896260109aa455247b22d0ba08cd20383f0f46de
Alan Smillie <alan.smillie@gmail.com>
2020-09-03 13:08:16 -0400

    Remove required fields

--- src/app/tasks/task-query-options.ts
+++ src/app/tasks/task-query-options.ts
@@ -1,8 +1,6 @@
 import { TaskSortOption } from './task-sort-option';
 
 export interface TaskQueryOptions {
-    limit: number;
-    page: number;
     sort?: TaskSortOption[];
     completed?: boolean;
     startCreatedAt?: Date;
