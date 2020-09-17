bc4f361c2c658fbc95a55af6e1582d0aa0bf5d10
Alan Smillie <alan.smillie@gmail.com>
2020-09-17 13:33:36 -0400

    Add unit tests

--- src/app/tasks/add-task/add-task.component.spec.ts
+++ src/app/tasks/add-task/add-task.component.spec.ts
@@ -1,4 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { of, Subscription, throwError } from 'rxjs';
+import { mockAppRepositoryService } from '../../../mocks/mock-app-repository-service';
+import { mockRouter } from '../../../mocks/mock-router';
+import { mockTaskRepositoryService } from '../../../mocks/mock-task-repository-service';
+import { mockTasks } from '../../../mocks/mock-tasks';
+import { SharedModule } from '../../shared/shared.module';
+import { Task } from '../task';
+import { TaskRepositoryService } from '../task-repository.service';
 
 import { AddTaskComponent } from './add-task.component';
 
@@ -8,7 +18,15 @@ describe('AddTaskComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddTaskComponent ]
+      imports: [
+        ReactiveFormsModule,
+        SharedModule,
+      ],
+      declarations: [ AddTaskComponent ],
+      providers: [
+        { provide: TaskRepositoryService, useValue: mockTaskRepositoryService as any },
+        { provide: Router, useValue: mockRouter as any },
+      ]
     })
     .compileComponents();
   }));
@@ -21,5 +39,81 @@ describe('AddTaskComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+    it('should call initForm method', () => {
+      const formSpy = jest.spyOn(component as any, 'initForm');
+
+      expect(formSpy).not.toHaveBeenCalled();
+      component.ngOnInit();
+      expect(formSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('ngOnDestroy', () => {
+    it('should call unsubscribe on subscription', () => {
+      component.addTaskSub = new Subscription();
+      const unsubSpy = jest.spyOn(component.addTaskSub, 'unsubscribe');
+
+
+      expect(unsubSpy).not.toHaveBeenCalled();
+      component.ngOnDestroy();
+      expect(unsubSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('onSubmit', () => {
+
+    it('should return on invalid form', () => {
+      expect(component.addTaskForm.status).toEqual('INVALID');
+      expect(mockTaskRepositoryService.add$).not.toHaveBeenCalled();
+      component.onSubmit();
+      expect(mockTaskRepositoryService.add$).not.toHaveBeenCalled();
+    });
+
+    describe('Valid Form Submission', () => {
+
+      const desc = 'Description';
+      const completed = false;
+      const newTask = new Task(desc, completed);
+
+      beforeEach(() => {
+        component.addTaskForm.get('description').setValue(desc);
+        component.addTaskForm.get('completed').setValue(completed);
+        component.errorMessage = '';
+      });
+
+      it('should call task repository add method with task', () => {
+        mockTaskRepositoryService.add$.mockReturnValue(of(mockTasks[0]));
+        mockTaskRepositoryService.search$.mockReturnValue(of(true));
+
+        expect(component.addTaskForm.status).toEqual('VALID');
+        component.onSubmit();
+        expect(mockTaskRepositoryService.add$).toHaveBeenCalledWith(newTask);
+      });
+
+      it('should set error message on no task returned by add method', () => {
+        mockTaskRepositoryService.add$.mockReturnValue(of(null));
+
+        component.onSubmit();
+        expect(component.errorMessage).not.toEqual('');
+      });
+
+      it('should set error message returned by search method', () => {
+        mockTaskRepositoryService.add$.mockReturnValue(of(mockTasks[0]));
+        mockTaskRepositoryService.search$.mockReturnValue(throwError('error'));
+
+        component.onSubmit();
+        expect(component.errorMessage).toEqual('error');
+      });
+
+      it('should set error message returned by add method', () => {
+        mockTaskRepositoryService.add$.mockReturnValue(throwError('error'));
+
+        component.onSubmit();
+        expect(component.errorMessage).toEqual('error');
+      });
+    });
   });
 });
