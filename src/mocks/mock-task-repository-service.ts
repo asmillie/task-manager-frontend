85b4dfc5da94c8358ee5eb531694b75ed6ce9e2a
Alan Smillie <alan.smillie@gmail.com>
2020-09-17 13:49:28 -0400

    Return observable for task query opts

--- src/mocks/mock-task-repository-service.ts
+++ src/mocks/mock-task-repository-service.ts
@@ -1,3 +1,15 @@
+import { of } from 'rxjs';
+import { SORT_DIR, SORT_FIELDS } from '../app/constants';
+import { TaskQueryOptions } from '../app/tasks/task-query-options';
+
+const default_tqo: TaskQueryOptions = {
+    sort: [
+        { field: SORT_FIELDS.completed, direction: SORT_DIR.asc },
+        { field: SORT_FIELDS.updatedAt, direction: SORT_DIR.desc },
+        { field: SORT_FIELDS.createdAt, direction: SORT_DIR.asc },
+      ]
+}
+
 export const mockTaskRepositoryService = {
     getNextPage$: jest.fn(),
     search$: jest.fn(),
@@ -6,5 +18,5 @@ export const mockTaskRepositoryService = {
     resetSearchOpts: jest.fn(),
     setSortOption: jest.fn(),
     setQueryOption: jest.fn(),
-    taskQueryOptions$: jest.fn(),
+    taskQueryOptions$: jest.fn().mockReturnValue(of(default_tqo)),
 };
