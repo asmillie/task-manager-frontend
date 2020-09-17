ed9e2b4d90cd563b7113c0d282982dd2b1263263
Alan Smillie <alan.smillie@gmail.com>
2020-09-17 10:54:18 -0400

    Remove mock for deleted service

--- src/mocks/mock-tasks-service.ts
+++
@@ -1,7 +0,0 @@
-import { BehaviorSubject } from 'rxjs';
-import { TaskQueryOptions } from '../app/tasks/task-query-options';
-
-export const mockTasksService: any = {
-    search$: jest.fn(),
-    taskQueryOptions: new BehaviorSubject<TaskQueryOptions>(null),
-}
