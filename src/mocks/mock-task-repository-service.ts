39f502d32306b44bb4b38f7269c54915dbc394af
Alan Smillie <alan.smillie@gmail.com>
2020-09-17 13:00:04 -0400

    Create mock for task repo service

---
+++ src/mocks/mock-task-repository-service.ts
@@ -0,0 +1,9 @@
+export const mockTaskRepositoryService = {
+    getNextPage$: jest.fn(),
+    search$: jest.fn(),
+    add$: jest.fn(),
+    refresh: jest.fn(),
+    resetSearchOpts: jest.fn(),
+    setSortOption: jest.fn(),
+    setQueryOption: jest.fn(),
+};
