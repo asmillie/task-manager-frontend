c19e6e296a98cb07dbb49592a749c28d4c667c27
Alan Smillie <alan.smillie@gmail.com>
2020-09-25 15:07:06 -0400

    Optimize imports

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { UserComponent } from './user.component';
 import { SharedModule } from '../shared/shared.module';
 import { UserService } from './user.service';
-import { UserRoutingModule } from './user-routing.module';
-
 
 
 @NgModule({
