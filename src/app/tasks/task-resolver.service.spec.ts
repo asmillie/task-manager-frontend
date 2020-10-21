1a65ace93b7e0d6a75770b17465ab401ae7695af
Alan Smillie <alan.smillie@gmail.com>
2020-10-21 11:40:45 -0400

    Add mock providers

--- src/app/tasks/task-resolver.service.spec.ts
+++ src/app/tasks/task-resolver.service.spec.ts
@@ -1,4 +1,8 @@
 import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { mockRouter } from '../../mocks/mock-router';
+import { mockTaskRepositoryService } from '../../mocks/mock-task-repository-service';
+import { TaskRepositoryService } from './task-repository.service';
 
 import { TaskResolverService } from './task-resolver.service';
 
@@ -6,7 +10,12 @@ describe('TaskResolverService', () => {
   let service: TaskResolverService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: TaskRepositoryService, useValue: mockTaskRepositoryService },
+        { provide: Router, useValue: mockRouter },
+      ]
+    });
     service = TestBed.inject(TaskResolverService);
   });
 
