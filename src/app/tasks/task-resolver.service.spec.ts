ae974911ed8a3c15a0f4432b837f7f477798d818
Alan Smillie <alan.smillie@gmail.com>
2020-10-22 09:15:50 -0400

    Optimize imports

--- src/app/tasks/task-resolver.service.spec.ts
+++ src/app/tasks/task-resolver.service.spec.ts
@@ -1,18 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
+import { EMPTY, of } from 'rxjs';
 import { mockRouter } from '../../mocks/mock-router';
-import { mockTaskRepositoryService } from '../../mocks/mock-task-repository-service';
 import { TaskRepositoryService } from './task-repository.service';
-
 import { TaskResolverService } from './task-resolver.service';
+
 
 describe('TaskResolverService', () => {
   let service: TaskResolverService;
+  const mockRepo = {
+    tasks$: of(null),
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
-        { provide: TaskRepositoryService, useValue: mockTaskRepositoryService },
+        { provide: TaskRepositoryService, useValue: mockRepo },
         { provide: Router, useValue: mockRouter },
       ]
     });
