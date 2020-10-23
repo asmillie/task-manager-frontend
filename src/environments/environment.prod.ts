13cc55183bb221c6ef31f8143f5fa91eed7b3eec
Alan Smillie <alan.smillie@gmail.com>
2020-10-23 14:23:42 -0400

    Add env vars and cmds for Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,28 @@
 export const environment = {
-  production: true
+  production: true,
+  taskApi: {
+    url: 'https://as-task-manager-api.herokuapp.com',
+    endpoint: {
+          signup: '/signup',
+          emailExists: '/signup/emailExists',
+          login: '/auth/login',
+          logout: '/auth/logout',
+          user: {
+            get: '/users/me',
+            patch: '/users/me',
+            delete: '/users/me',
+            avatar: {
+              post: '/users/me/avatar',
+              get: '/users/me/avatar.png',
+              delete: '/users/me/avatar'
+            }
+          },
+          tasks: {
+            get: '/tasks/search',
+            add: '/tasks',
+            patch: '/tasks',
+            delete: '/tasks'
+          }
+    }
+}
 };
