775b426d6e7d3ec7c543188957917280fcbb4f5c
Alan Smillie <alan.smillie@gmail.com>
2020-11-03 09:51:40 -0500

    Bugfix: Misplaced curly brace

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -26,8 +26,8 @@ export const environment = {
             delete: '/tasks'
           }
     },
-    reCaptcha: {
-      public_key: '6Lcj4NwZAAAAAPBOABiTalzK3fohYEHhZ1wrDOcu',
-    }
-}
+  },
+  reCaptcha: {
+    public_key: '6Lcj4NwZAAAAAPBOABiTalzK3fohYEHhZ1wrDOcu',
+  }
 };
