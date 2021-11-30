6b238addff3b84ea3e8e3a13d9e33eb4e39f4bd2
Alan Smillie <48022778+asmillie@users.noreply.github.com>
2021-11-30 11:29:51 -0500

    Handle Auth0 Login Errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
+import { Subscription } from 'rxjs';
+import { filter, mergeMap, tap } from 'rxjs/operators';
 import { fadeInAnimation } from './animations';
 
 @Component({
@@ -10,12 +13,18 @@ import { fadeInAnimation } from './animations';
     fadeInAnimation,
   ]
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'task-manager';
+  errorSub: Subscription;
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.initErrorObs();
+  }
+
+  ngOnDestroy(): void {
+    this.errorSub.unsubscribe();
   }
 
   prepareOutlet(outlet: RouterOutlet) {
@@ -23,5 +32,13 @@ export class AppComponent implements OnInit {
       return false;
     }
     return outlet && outlet.activatedRoute && outlet.activatedRoute.snapshot.url.toString();
+  }
+
+  private initErrorObs() {
+    this.errorSub = this.authService.error$.pipe(
+      tap(e => console.log(e.message.toLowerCase())),
+      filter((e) => e.message.toLowerCase() === 'login required'),
+      mergeMap(() => this.authService.getAccessTokenSilently())
+    ).subscribe();
   }
 }
