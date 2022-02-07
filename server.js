b429d60abb871f2dd6229fbf03f1884b15794733
Alan Smillie <48022778+asmillie@users.noreply.github.com>
2022-02-07 09:58:04 -0500

    Change default server port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = parseInt(process.env.PORT, 10) || 8080
+const port = parseInt(process.env.PORT, 10) || 4200
 
 app.use(express.static('task-manager'));
 
