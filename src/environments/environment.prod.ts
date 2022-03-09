81a13afbc7ead2f73b4a720b5be55fde1a473423
Alan Smillie <48022778+asmillie@users.noreply.github.com>
2022-03-09 12:05:35 -0500

    Update redirect URI

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   auth0: {
     domain: 'dev-x4xgby3m.us.auth0.com',
     clientId: 'BAJyPhJIv9IPoE96qI3qWrDQQFq65rtH',
-    redirectUri: 'http://localhost:4200/tasks'
+    redirectUri: 'https://task-manager-frontend-dev.azurewebsites.net/tasks'
   },
   taskApi: {
     url: 'https://task-manager-dev.azurewebsites.net',
